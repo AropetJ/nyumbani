3336e888a1d6e99df0cb1ad87f3dc49251103f39
AropetJ <aropetjoel32@gmail.com>
2024-03-19 14:00:26 +0300

    Created an http server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import http from 'http';
 
-server = http.createServer(async (req, res) => {
+server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     res.statusCode(200);
     res.end();
-  }
+  };
+});
+
+server.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
 });
